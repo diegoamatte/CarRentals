fd2e7aba07c56425d9ab1553eb4600fb04cc6a92
Diego Amatte <amattediegoomar@gmail.com>
2022-02-22 13:42:39 -0300

    Add CarValidator Tests

---
+++ CarRentalsTests/Models/CarValidatorTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using CarRentals.Models;
+
+namespace CarRentalsTests.Models
+{
+    public class CarValidatorTests
+    {
+        private Car _car;
+        private readonly CarValidator _carValidator;
+
+        public CarValidatorTests()
+        {
+            _car = new Car { 
+                Brand = "Brand", 
+                Id = Guid.NewGuid(), 
+                Model = "ValidModel", 
+                Type = "Valid type",
+                LicensePlate = "AAA-555",
+            };
+            _carValidator = new CarValidator();
+        }
+
+        [Fact]
+        public void Validator_IsnotValid_WhenFieldsAreEmpty()
+        {
+            //Arrange
+            var car = new Car();
+
+            //Act
+            var result = _carValidator.Validate(car);
+
+            //Assert
+            Assert.False(result.IsValid);
+        }
+
+        [Fact]
+        public void Validator_IsNotValid_WhenLicensePlateHasNotMinimumLengthRequired()
+        {
+            //Arrange
+            _car.LicensePlate = "AAAAA";
+
+            //Act
+            var result = _carValidator.Validate(_car);
+
+            //Assert
+            Assert.False(result.IsValid);
+        }
+
+        
+    }
+}
