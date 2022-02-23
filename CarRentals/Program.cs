976938b8161adf857a8e62c4eda3922c1c96449d
Diego Amatte <amattediegoomar@gmail.com>
2022-02-23 13:19:08 -0300

    Add configuration
    -FluentValidation integration to Swagger UI

--- CarRentals/Program.cs
+++ CarRentals/Program.cs
@@ -1,16 +1,36 @@
 using CarRentals.Models;
 using CarRentals.Services;
-using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using ZymLabs.NSwag.FluentValidation;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddControllers()
+    .AddFluentValidation(options =>
+    {
+        options.ImplicitlyValidateChildProperties = true;
+        options.ImplicitlyValidateRootCollectionElements = true;
+        options.RegisterValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
-builder.Services.AddControllers();
+    });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerDocument();
+builder.Services.AddSwaggerDocument((config, provider) =>
+{
+    config.PostProcess = document =>
+    {
+        document.Info.Version = "v1";
+        document.Info.Title = "Car Rentals API";
+        document.Info.Description = "An API for concepts practice";
+    };
+    var fluentValidationProcessor = provider.CreateScope().ServiceProvider.GetService<FluentValidationSchemaProcessor>();
+    config.SchemaProcessors.Add(fluentValidationProcessor);
+});
 //Add in memory Database
 builder.Services.AddDbContext<CarRentalDbContext>(options =>
 {
@@ -21,7 +41,8 @@ builder.Services.AddScoped<ICarRentalDbContext>(provider => provider.GetService<
 
 builder.Services.AddScoped<ICarService, CarService>();
 
-builder.Services.AddTransient<IValidator<Car>, CarValidator>();
+builder.Services.AddScoped<FluentValidationSchemaProcessor>();
+
 
 var app = builder.Build();
 
