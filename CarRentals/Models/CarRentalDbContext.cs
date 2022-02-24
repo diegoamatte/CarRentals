8b3a3dce494e10827975485ae4395e60f3c3d8d3
Diego Amatte <amattediegoomar@gmail.com>
2022-02-24 14:00:23 -0300

    Update CarRentalDbContext

--- CarRentals/Models/CarRentalDbContext.cs
+++ CarRentals/Models/CarRentalDbContext.cs
@@ -10,5 +10,6 @@ namespace CarRentals.Models
         }
         public DbSet<Car> Cars { get; set; }
         public DbSet<Client> Clients { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
     }
 }
