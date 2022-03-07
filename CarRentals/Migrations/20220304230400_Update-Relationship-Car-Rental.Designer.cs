223f362620533801f72b90464182d8f4576de786
Diego Amatte <amattediegoomar@gmail.com>
2022-03-07 14:22:17 -0300

    Updates migration
    Now supports many to many relationship between cars and rentals

--- CarRentals/Migrations/20220304230400_Update-Relationship-Car-Rental.Designer.cs
+++ CarRentals/Migrations/20220304230400_Update-Relationship-Car-Rental.Designer.cs
@@ -4,6 +4,7 @@ using CarRentals.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CarRentals.Migrations
 {
     [DbContext(typeof(CarRentalDbContext))]
-    partial class CarRentalDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220304230400_Update-Relationship-Car-Rental")]
+    partial class UpdateRelationshipCarRental
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CarRentals/Migrations/20220304230400_Update-Relationship-Car-Rental.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarRentals.Migrations
+{
+    public partial class UpdateRelationshipCarRental : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cars_Rentals_RentalId",
+                table: "Cars");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cars_RentalId",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "RentalId",
+                table: "Cars");
+
+            migrationBuilder.CreateTable(
+                name: "CarRental",
+                columns: table => new
+                {
+                    RentalsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RentedCarsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarRental", x => new { x.RentalsId, x.RentedCarsId });
+                    table.ForeignKey(
+                        name: "FK_CarRental_Cars_RentedCarsId",
+                        column: x => x.RentedCarsId,
+                        principalTable: "Cars",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CarRental_Rentals_RentalsId",
+                        column: x => x.RentalsId,
+                        principalTable: "Rentals",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarRental_RentedCarsId",
+                table: "CarRental",
+                column: "RentedCarsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CarRental");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "RentalId",
+                table: "Cars",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cars_RentalId",
+                table: "Cars",
+                column: "RentalId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cars_Rentals_RentalId",
+                table: "Cars",
+                column: "RentalId",
+                principalTable: "Rentals",
+                principalColumn: "Id");
+        }
+    }
+}
