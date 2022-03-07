042496d6418348076719a08de5f7ff2da9fc007d
Diego Amatte <amattediegoomar@gmail.com>
2022-03-07 14:20:28 -0300

    Update models

--- CarRentals/Models/Car.cs
+++ CarRentals/Models/Car.cs
@@ -2,22 +2,12 @@
 {
     public class Car
     {
-        public Guid? Id { get; set; }
+        public Guid Id { get; set; }
         public string LicensePlate { get; set; }
         public string Brand { get; set; }
         public string Model { get; set; }
         public string Type { get; set; }
         public CarState State { get; set; }
-
-        public override bool Equals(object? obj)
-        {
-            if(obj == null)
-                return false;
-            if (ReferenceEquals(this, obj))
-            {
-                return true;
-            }
-            return LicensePlate == ((Car)obj).LicensePlate;
-        }
+        public ICollection<Rental> Rentals { get; set; }
     }
 }
--- CarRentals/Models/Client.cs
+++ CarRentals/Models/Client.cs
@@ -7,22 +7,5 @@
         public string Name { get; set; }
         public int DNI { get; set; }
         public string Address { get; set; }
-
-        // override object.Equals
-        public override bool Equals(object obj)
-        {
-            if (obj == null || GetType() != obj.GetType())
-            {
-                return false;
-            }
-
-            return DNI == ((Client)obj).DNI;
-        }
-
-        // override object.GetHashCode
-        public override int GetHashCode()
-        {
-            return base.GetHashCode();
-        }
     }
 }
